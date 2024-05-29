611b599ac414efd22a073d169fea1bd1968ee86c
dejvinczi <dejvinczi.main@gmail.com>
2024-05-29 15:45:38 +0200

    test: Create Genre model tests

--- api/apps/books/tests/test_models.py
+++ api/apps/books/tests/test_models.py
@@ -1,5 +1,5 @@
 import pytest
-from ..models import Author
+from ..models import Author, Genre
 
 
 @pytest.mark.django_db
@@ -29,3 +29,28 @@ class TestAuthorModel:
     def test_str(self, author):
         """Test that the Author model returns the correct string representation."""
         assert f"{author.first_name} {author.last_name}" == str(author)
+
+
+@pytest.mark.django_db
+class TestGenreModel:
+    """Tests for the Genre model in the system."""
+
+    @pytest.fixture
+    def genre_data(self):
+        return {"name": "Test Genre"}
+
+    @pytest.fixture
+    def genre(self, genre_data):
+        return Genre.objects.create(**genre_data)
+
+    def test_create_genre(self, genre_data):
+        """Test that the Genre model can be created."""
+        genre = Genre.objects.create(**genre_data)
+        genres = Genre.objects.all()
+
+        assert genres.count() == 1
+        assert genres.first() == genre
+
+    def test_str(self, genre):
+        """Test that the Genre model returns the correct string representation."""
+        assert genre.name == str(genre)
