4ad00a3a1eac56c8c64787eecb06301d97b76c1e
dejvinczi <dejvinczi.main@gmail.com>
2024-05-29 13:14:41 +0200

    feat: Add books application urls

--- api/config/urls/base.py
+++ api/config/urls/base.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("apps.accounts.urls")),
+    path("api/", include("apps.books.urls")),
 ]
