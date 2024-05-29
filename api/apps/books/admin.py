2fc3c46830843d808b0094b3224f7cdbada3abe0
dejvinczi <dejvinczi.main@gmail.com>
2024-05-29 13:00:38 +0200

    feat: Register Author and Genre models on admin site

--- api/apps/books/admin.py
+++ api/apps/books/admin.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
 from . import models
 
-class BookAdmin(admin.ModelAdmin):
+
+class AuthorAdmin(admin.ModelAdmin):
+    """
+    Admin class for the Author model.
+    """
+
     pass
 
 
+class GenreAdmin(admin.ModelAdmin):
+    """
+    Admin class for the Genre model.
+    """
+
+    pass
+
+
+class BookAdmin(admin.ModelAdmin):
+    """
+    Admin class for the Book model.
+    """
+
+    pass
+
+
+admin.site.register(models.Author, AuthorAdmin)
+admin.site.register(models.Genre, GenreAdmin)
 admin.site.register(models.Book, BookAdmin)
