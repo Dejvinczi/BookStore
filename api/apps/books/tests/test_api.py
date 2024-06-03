5ad843965024578b2a518959d8c23d29076a5745
dejvinczi <dejvinczi.main@gmail.com>
2024-06-03 14:34:39 +0200

    test: Create genre API tests

--- api/apps/books/tests/test_api.py
+++ api/apps/books/tests/test_api.py
@@ -80,3 +80,74 @@ class TestAuthorViewSet:
         """Test that an author can be deleted for users that are admin."""
         response = admin_api_client.delete(self._get_book_detail_url(author.id))
         assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+@pytest.mark.django_db
+class TestGenreViewSet:
+    """Tests for the GenreViewSet."""
+
+    GENRE_LIST_URL = reverse("books:genre-list")
+
+    def _get_book_detail_url(self, id):
+        return reverse("books:genre-detail", args=[id])
+
+    def test_list_sucess(self, api_client):
+        """Test that the list of genres can be retrieved."""
+        response = api_client.get(self.GENRE_LIST_URL)
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_retrieve_sucess(self, api_client, genre):
+        """Test that a genre can be retrieved."""
+        response = api_client.get(self._get_book_detail_url(genre.id))
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_create_not_as_admin_fail(self, auth_api_client):
+        """Test that a genre can't be created for users that are not admin."""
+        response = auth_api_client.post(self.GENRE_LIST_URL, {})
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_update_not_as_admin_fail(self, auth_api_client, genre):
+        """Test that a genre can't be updated for users that are not admin."""
+        response = auth_api_client.put(
+            self._get_book_detail_url(genre.id), {}, format="json"
+        )
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_part_update_not_as_admin_fail(self, auth_api_client, genre):
+        """Test that a genre can't be partially updated for users that are not admin."""
+        response = auth_api_client.patch(
+            self._get_book_detail_url(genre.id), {}, format="json"
+        )
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_delete_not_as_admin_fail(self, auth_api_client, genre):
+        """Test that a genre can't be deleted for users that are not admin."""
+        response = auth_api_client.delete(self._get_book_detail_url(genre.id))
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_create_as_admin_success(self, admin_api_client):
+        """Test that a genre can be created for users that are admin."""
+        payload = {"name": "Test"}
+        response = admin_api_client.post(self.GENRE_LIST_URL, payload, format="json")
+        assert response.status_code == status.HTTP_201_CREATED
+
+    def test_update_as_admin_success(self, admin_api_client, genre):
+        """Test that a genre can be updated for users that are admin."""
+        payload = {"name": "Test"}
+        response = admin_api_client.put(
+            self._get_book_detail_url(genre.id), payload, format="json"
+        )
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_part_update_as_admin_success(self, admin_api_client, genre):
+        """Test that a genre can be partially updated for users that are admin."""
+        payload = {"name": "Test"}
+        response = admin_api_client.patch(
+            self._get_book_detail_url(genre.id), payload, format="json"
+        )
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_delete_as_admin_success(self, admin_api_client, genre):
+        """Test that a genre can be deleted for users that are admin."""
+        response = admin_api_client.delete(self._get_book_detail_url(genre.id))
+        assert response.status_code == status.HTTP_204_NO_CONTENT
