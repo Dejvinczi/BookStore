1c1c9e33c63b36975f5b8f2d6e23c3d0788df42f
dejvinczi <dejvinczi.main@gmail.com>
2024-06-14 15:40:59 +0200

    test: Create AuthorsViewSet tests for filtering and ordering

--- api/apps/books/tests/test_api.py
+++ api/apps/books/tests/test_api.py
@@ -12,20 +12,125 @@ class TestAuthorViewSet:
     def _get_book_detail_url(self, id):
         return reverse("books:author-detail", args=[id])
 
-    def test_list_sucess(self, api_client):
+    def test_list_success(self, api_client):
         """Test that the list of authors can be retrieved."""
         response = api_client.get(self.AUTHOR_LIST_URL)
         assert response.status_code == status.HTTP_200_OK
 
-    def test_list_success_paginated(self, api_client, author_factory):
+    def test_list_success_paginated_success(self, api_client, author_factory):
         """Test that the list of authors can be retrieved with pagination."""
         author_factory.create_batch(5)
-        response = api_client.get(f"{self.AUTHOR_LIST_URL}?limit=1")
+        response = api_client.get(
+            f"{self.AUTHOR_LIST_URL}",
+            {"limit": 1},
+        )
         assert response.status_code == status.HTTP_200_OK
         assert response.data["count"] == 5
         assert len(response.data["results"]) == 1
 
-    def test_retrieve_sucess(self, api_client, author):
+    def test_list_with_filter_first_name_success(self, api_client, author_factory):
+        """Test that the list of authors can be filtered by first name."""
+        authors = author_factory.create_batch(5)
+        first_author = authors[0]
+        response = api_client.get(
+            f"{self.AUTHOR_LIST_URL}",
+            {"first_name": first_author.first_name},
+        )
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["count"] == 1
+        assert len(response.data["results"]) == 1
+
+    def test_list_with_filter_last_name_success(self, api_client, author_factory):
+        """Test that the list of authors can be filtered by last name."""
+        authors = author_factory.create_batch(5)
+        first_author = authors[0]
+        response = api_client.get(
+            f"{self.AUTHOR_LIST_URL}",
+            {"last_name": first_author.last_name},
+        )
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["count"] == 1
+        assert len(response.data["results"]) == 1
+
+    def test_list_with_filter_date_of_birth_success(self, api_client, author_factory):
+        """Test that the list of authors can be filtered by date of birth."""
+        authors = [
+            author_factory.create(date_of_birth=f"2000-01-0{idx}")
+            for idx in range(1, 6)
+        ]
+        first_author = authors[0]
+
+        response = api_client.get(
+            f"{self.AUTHOR_LIST_URL}",
+            {
+                "date_of_birth_after": first_author.date_of_birth,
+                "date_of_birth_before": first_author.date_of_birth,
+            },
+        )
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["count"] == 1
+        assert len(response.data["results"]) == 1
+
+    def test_list_with_ordering_by_first_name_success(self, api_client, author_factory):
+        """Test that the list of authors can be ordered by first name."""
+        authors = author_factory.create_batch(5)
+        ordered_authors = sorted(authors, key=lambda author: author.first_name)
+
+        response = api_client.get(
+            f"{self.AUTHOR_LIST_URL}",
+            {"ordering": "first_name"},
+        )
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["results"][0]["id"] == ordered_authors[0].id
+
+        response = api_client.get(
+            f"{self.AUTHOR_LIST_URL}",
+            {"ordering": "-first_name"},
+        )
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["results"][-1]["id"] == ordered_authors[0].id
+
+    def test_list_with_ordering_by_last_name_success(self, api_client, author_factory):
+        """Test that the list of authors can be ordered by last name."""
+        authors = author_factory.create_batch(5)
+        ordered_authors = sorted(authors, key=lambda author: author.last_name)
+
+        response = api_client.get(
+            f"{self.AUTHOR_LIST_URL}",
+            {"ordering": "last_name"},
+        )
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["results"][0]["id"] == ordered_authors[0].id
+
+        response = api_client.get(
+            f"{self.AUTHOR_LIST_URL}",
+            {"ordering": "-last_name"},
+        )
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["results"][-1]["id"] == ordered_authors[0].id
+
+    def test_list_with_ordering_by_date_of_birth_success(
+        self, api_client, author_factory
+    ):
+        """Test that the list of authors can be ordered by date of birth."""
+        authors = author_factory.create_batch(5)
+        ordered_authors = sorted(authors, key=lambda author: author.date_of_birth)
+
+        response = api_client.get(
+            f"{self.AUTHOR_LIST_URL}",
+            {"ordering": "date_of_birth"},
+        )
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["results"][0]["id"] == ordered_authors[0].id
+
+        response = api_client.get(
+            f"{self.AUTHOR_LIST_URL}",
+            {"ordering": "-date_of_birth"},
+        )
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["results"][-1]["id"] == ordered_authors[0].id
+
+    def test_retrieve_success(self, api_client, author):
         """Test that an author can be retrieved."""
         response = api_client.get(self._get_book_detail_url(author.id))
         assert response.status_code == status.HTTP_200_OK
@@ -99,7 +204,7 @@ class TestGenreViewSet:
     def _get_book_detail_url(self, id):
         return reverse("books:genre-detail", args=[id])
 
-    def test_list_sucess(self, api_client):
+    def test_list_success(self, api_client):
         """Test that the list of genres can be retrieved."""
         response = api_client.get(self.GENRE_LIST_URL)
         assert response.status_code == status.HTTP_200_OK
@@ -112,7 +217,7 @@ class TestGenreViewSet:
         assert response.data["count"] == 5
         assert len(response.data["results"]) == 1
 
-    def test_retrieve_sucess(self, api_client, genre):
+    def test_retrieve_success(self, api_client, genre):
         """Test that a genre can be retrieved."""
         response = api_client.get(self._get_book_detail_url(genre.id))
         assert response.status_code == status.HTTP_200_OK
@@ -178,7 +283,7 @@ class TestBookViewSet:
     def _get_book_detail_url(self, id):
         return reverse("books:book-detail", args=[id])
 
-    def test_list_sucess(self, api_client):
+    def test_list_success(self, api_client):
         """Test that the list of books can be retrieved."""
         response = api_client.get(self.BOOK_LIST_URL)
         assert response.status_code == status.HTTP_200_OK
@@ -191,7 +296,7 @@ class TestBookViewSet:
         assert response.data["count"] == 5
         assert len(response.data["results"]) == 1
 
-    def test_retrieve_sucess(self, api_client, book):
+    def test_retrieve_success(self, api_client, book):
         """Test that a book can be retrieved."""
         response = api_client.get(self._get_book_detail_url(book.id))
         assert response.status_code == status.HTTP_200_OK
