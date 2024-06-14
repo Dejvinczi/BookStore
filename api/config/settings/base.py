d692e4f11d89e9b4f1f7eb6bd7fa8f077709a266
dejvinczi <dejvinczi.main@gmail.com>
2024-06-14 10:29:23 +0200

    Revert "feat: Add pagination setup to settings"
    This reverts commit fc04ebee2bb499f21fb1ef6c86bfee4c4cd40e63.

--- api/config/settings/base.py
+++ api/config/settings/base.py
@@ -169,7 +169,6 @@ REST_FRAMEWORK = {
     "DEFAULT_PARSER_CLASSES": [
         "rest_framework.parsers.JSONParser",
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
 }
 
 APPEND_SLASH = False
