fc04ebee2bb499f21fb1ef6c86bfee4c4cd40e63
dejvinczi <dejvinczi.main@gmail.com>
2024-06-14 10:27:14 +0200

    feat: Add pagination setup to settings

--- api/config/settings/base.py
+++ api/config/settings/base.py
@@ -169,6 +169,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PARSER_CLASSES": [
         "rest_framework.parsers.JSONParser",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
 }
 
 APPEND_SLASH = False
