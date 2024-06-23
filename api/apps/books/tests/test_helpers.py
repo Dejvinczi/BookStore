498125151a2e2ca0de55c6c7e900432221c856e3
dejvinczi <dejvinczi.main@gmail.com>
2024-06-23 17:16:02 +0200

    test: Create test for Book image field upload helper function

---
+++ api/apps/books/tests/test_helpers.py
@@ -0,0 +1,9 @@
+from ..helpers import book_image_upload_to_path
+from unittest.mock import Mock
+
+
+def test_book_image_upload_to_path():
+    """Test that the book image upload path is correct."""
+    mock_instance = Mock()
+    mock_instance.id = 5
+    assert book_image_upload_to_path(mock_instance, "test.png") == "books/5/test.png"
