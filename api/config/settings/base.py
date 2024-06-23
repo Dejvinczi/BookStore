757927c724dbf17cccbf8a1421b167bcd570c1d8
dejvinczi <dejvinczi.main@gmail.com>
2024-06-23 13:23:17 +0200

    feat: Add file/image managing packages + setup Django config for files cleanup

--- api/config/settings/base.py
+++ api/config/settings/base.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # External
     "django_filters",
+    "django_cleanup",
     "rest_framework",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
@@ -146,7 +147,7 @@ STATIC_URL = "/static/static/"
 MEDIA_URL = "/static/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- api/config/urls/development.py
+++ api/config/urls/development.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
@@ -22,3 +24,5 @@ urlpatterns = base_urlpatterns + [
         name="redoc",
     ),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
