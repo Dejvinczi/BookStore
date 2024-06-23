365364f8e77a67caf37b65e8bc17ecd36ed7e364
dejvinczi <dejvinczi.main@gmail.com>
2024-06-23 21:08:58 +0200

    refactor(test): Change pagination from url concat str to dict param (just for one convention creating requests)

--- api/apps/books/tests/test_api.py
+++ api/apps/books/tests/test_api.py
@@ -21,10 +21,7 @@ class TestAuthorViewSet:
     def test_list_success_paginated_success(self, api_client, author_factory):
         """Test that the list of authors can be retrieved with pagination."""
         author_factory.create_batch(5)
-        response = api_client.get(
-            f"{self.AUTHOR_LIST_URL}",
-            {"limit": 1},
-        )
+        response = api_client.get(self.AUTHOR_LIST_URL, {"limit": 1})
         assert response.status_code == status.HTTP_200_OK
         assert response.data["count"] == 5
         assert len(response.data["results"]) == 1
@@ -219,7 +216,7 @@ class TestGenreViewSet:
     def test_list_success_paginated(self, api_client, genre_factory):
         """Test that the list of genres can be retrieved with pagination."""
         genre_factory.create_batch(5)
-        response = api_client.get(f"{self.GENRE_LIST_URL}?limit=1")
+        response = api_client.get(self.GENRE_LIST_URL, {"limit": 1})
         assert response.status_code == status.HTTP_200_OK
         assert response.data["count"] == 5
         assert len(response.data["results"]) == 1
@@ -332,7 +329,7 @@ class TestBookViewSet:
     def test_list_success_paginated(self, api_client, book_factory):
         """Test that the list of books can be retrieved with pagination."""
         book_factory.create_batch(5)
-        response = api_client.get(f"{self.BOOK_LIST_URL}?limit=1")
+        response = api_client.get(self.BOOK_LIST_URL, {"limit": 1})
         assert response.status_code == status.HTTP_200_OK
         assert response.data["count"] == 5
         assert len(response.data["results"]) == 1
