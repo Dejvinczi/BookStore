5d902901e904644a7cde7e93131f0368a6fc7f79
dejvinczi <dejvinczi.main@gmail.com>
2024-06-23 13:26:28 +0200

    feat: Create upload image custom action + change Router to trail_slash=false

--- api/apps/books/urls.py
+++ api/apps/books/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=False)
 router.register("authors", views.AuthorViewSet, basename="author")
 router.register("genres", views.GenreViewSet, basename="genre")
 router.register("books", views.BookViewSet, basename="book")
--- api/apps/books/views.py
+++ api/apps/books/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, response, status
+from rest_framework.decorators import action
 from apps.core.permissions import IsAdminOrReadOnly
 from .models import Author, Genre, Book
 from .serializers import (
@@ -6,6 +7,7 @@ from .serializers import (
     GenreSerializer,
     BookSerializer,
     BookDetailSerializer,
+    BookUploadImageSerializer,
 )
 from .filters import AuthorFilter
 
@@ -39,4 +41,20 @@ class BookViewSet(viewsets.ModelViewSet):
         """Get serializer class based on action."""
         if self.action == "list":
             return self.serializer_class
+        if self.action == "upload_image":
+            return BookUploadImageSerializer
         return self.detail_serializer_class
+
+    @action(
+        detail=True,
+        methods=["put"],
+        url_path="upload-image",
+        serializer_class=BookUploadImageSerializer,
+    )
+    def upload_image(self, request, *args, **kwargs):
+        """Upload an image to the Book model."""
+        book = self.get_object()
+        serializer = self.get_serializer(book, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return response.Response(serializer.data, status=status.HTTP_200_OK)
