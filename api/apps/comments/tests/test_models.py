e79bf982eef0646e80be42e2916f6811a4b334aa
dejvinczi <dejvinczi.main@gmail.com>
2024-06-23 22:34:12 +0200

    test: Create Comments model tests

---
+++ api/apps/comments/tests/test_models.py
@@ -0,0 +1,25 @@
+import pytest
+from django.utils import timezone
+from apps.books.tests.factories import BookFactory
+from ..models import Comment
+
+
+@pytest.mark.django_db
+class TestCommentModel:
+    """Tests for the Comment model in the system."""
+
+    def test_create_comment(self, user):
+        """Test that the Comment model can be created."""
+        book = BookFactory()
+        comment_data = {
+            "text": "Test Comment text",
+            "book": book,
+            "user": user,
+        }
+        comment = Comment.objects.create(**comment_data)
+
+        assert comment.text == comment_data["text"]
+        assert comment.book == comment_data["book"]
+        assert comment.user == comment_data["user"]
+        assert comment.created_at is not None
+        assert comment.updated_at is not None
