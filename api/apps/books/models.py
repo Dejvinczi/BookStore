a357de416e4709db3ca26e4d6a0abb5f29263dee
dejvinczi <dejvinczi.main@gmail.com>
2024-06-24 13:35:48 +0200

    feat: Add inheritance from TimeStampedModel class to Author, Genre and Book model

--- api/apps/books/models.py
+++ api/apps/books/models.py
@@ -1,20 +1,22 @@
 from django.db import models
-from apps.core import validators
+from django.core.validators import MinValueValidator
+from apps.core.models import TimeStampedModel
+from apps.core.validators import date_cannot_be_in_future
 from .helpers import book_image_upload_to_path
 
 
-class Author(models.Model):
+class Author(TimeStampedModel):
     """Author model in the system."""
 
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    date_of_birth = models.DateField(validators=[validators.date_cannot_be_in_future])
+    date_of_birth = models.DateField(validators=[date_cannot_be_in_future])
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
 
-class Genre(models.Model):
+class Genre(TimeStampedModel):
     """Genre model in the system."""
 
     name = models.CharField(max_length=255, unique=True)
@@ -23,14 +25,12 @@ class Genre(models.Model):
         return self.name
 
 
-class Book(models.Model):
+class Book(TimeStampedModel):
     """Book model in the system."""
 
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    publication_date = models.DateField(
-        validators=[validators.date_cannot_be_in_future]
-    )
+    publication_date = models.DateField(validators=[date_cannot_be_in_future])
     authors = models.ManyToManyField(Author, related_name="books")
     genres = models.ManyToManyField(Genre, related_name="books")
     image = models.ImageField(
