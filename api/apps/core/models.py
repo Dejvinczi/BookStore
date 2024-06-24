fcbaef8c9ecb09a7de4ab914136f551c6962384b
dejvinczi <dejvinczi.main@gmail.com>
2024-06-24 13:32:50 +0200

    feat: Create abstract TimeStampedModel for store created and updated date-times

--- api/apps/core/models.py
+++ api/apps/core/models.py
@@ -1,1 +1,11 @@
-from django.db import models  # noqa
+from django.db import models
+
+
+class TimeStampedModel(models.Model):
+    """Abstract model with timestamp fields."""
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
