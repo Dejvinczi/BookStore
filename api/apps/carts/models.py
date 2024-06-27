547af0ccb544376245103b0cc63fa6105470b983
dejvinczi <dejvinczi.main@gmail.com>
2024-06-27 17:40:17 +0200

    refactor: Change relations on CartItem and Cart models

--- api/apps/carts/models.py
+++ api/apps/carts/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.conf import settings
 from apps.core.models import TimeStampedModel
 
 
 class Cart(TimeStampedModel):
     """Shop cart model in the system."""
 
-    user = models.OneToOneField("accounts.User", on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="cart",
+    )
 
     def __str__(self):
         return f"{self.user.username}'s cart"
@@ -14,6 +19,10 @@ class Cart(TimeStampedModel):
 class CartItem(TimeStampedModel):
     """Shop cart item model in the system."""
 
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
-    book = models.ForeignKey("books.Book", on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    book = models.ForeignKey(
+        "books.Book",
+        on_delete=models.CASCADE,
+        related_name="cart_items",
+    )
     quantity = models.PositiveIntegerField(default=1)
