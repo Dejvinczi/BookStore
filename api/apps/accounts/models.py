fffdb3d4deb662c6889c945ebafe72c27a1d4ddd
dejvinczi <dejvinczi.main@gmail.com>
2024-06-27 17:44:25 +0200

    feat: Create custom manager for User model with create_user_with_cart method

--- api/apps/accounts/models.py
+++ api/apps/accounts/models.py
@@ -1,7 +1,23 @@
-from django.contrib.auth.models import AbstractUser
+from django.db import transaction
+from django.contrib.auth.models import AbstractUser, UserManager
+from apps.carts.models import Cart
+
+
+class CustomUserManager(UserManager):
+    """Custom user manager."""
+
+    def create_user_with_cart(self, username, email, password, **extra_fields):
+        """Create and save a user with the given username, email, and password."""
+        with transaction.atomic():
+            user = self.create_user(username, email, password, **extra_fields)
+            Cart.objects.create(user=user)
+            return user
 
 
 class User(AbstractUser):
     """User model in the system."""
 
-    pass
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
