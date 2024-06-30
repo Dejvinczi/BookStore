ebd4048ce83c4b5d62f30f186d361587c1999439
dejvinczi <dejvinczi.main@gmail.com>
2024-06-30 20:26:07 +0200

    test: Create Cart model tests for str and total_price

--- api/apps/carts/tests/test_models.py
+++ api/apps/carts/tests/test_models.py
@@ -13,6 +13,21 @@ class TestCartModel:
         assert cart.id is not None
         assert cart.user == user
 
+    def test_str(self, user):
+        """Test that the Cart model returns the correct string representation."""
+        cart_data = {"user": user}
+        cart = Cart.objects.create(**cart_data)
+        assert str(cart) == f"{user.username}'s cart"
+
+    def test_total_price(self, cart, book_factory):
+        """Test that the total price is calculated correctly."""
+        books = book_factory.create_batch(2)
+        cart_item_arr = []
+        for book in books:
+            cart_item_arr.append(CartItem(cart=cart, book=book, quantity=3))
+        CartItem.objects.bulk_create(cart_item_arr)
+        assert cart.total_price == sum(book.price * 3 for book in books)
+
 
 @pytest.mark.django_db
 class TestCartItemModel:
