24e9f297dda40cefb34a81f94399e8e12b4dc7da
dejvinczi <dejvinczi.main@gmail.com>
2024-08-29 12:12:32 +0200

    feat: Register order models in admin panel

--- api/apps/orders/admin.py
+++ api/apps/orders/admin.py
@@ -1,1 +1,18 @@
-from django.contrib import admin  # NOQA
+from django.contrib import admin
+from . import models
+
+
+class OrderAdmin(admin.ModelAdmin):
+    """Admin class for the Order model."""
+
+    pass
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    """Admin class for the OrderItem model."""
+
+    pass
+
+
+admin.site.register(models.Order, OrderAdmin)
+admin.site.register(models.OrderItem, OrderItemAdmin)
