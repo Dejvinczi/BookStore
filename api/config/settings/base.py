d2f062750d64c532e71fcfb0b81cc8039f422d1d
dejvinczi <dejvinczi.main@gmail.com>
2024-09-03 16:58:34 +0200

    refactor: Change urls of statics and media + add media to ingore file

--- api/config/settings/base.py
+++ api/config/settings/base.py
@@ -145,8 +145,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/static/"
-MEDIA_URL = "/static/media/"
+STATIC_URL = "/static/"
+MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
