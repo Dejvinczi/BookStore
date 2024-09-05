2648c752a356d450185dd7204f96692b6fbe90fa
Dejvinczi <dejvinczi.main@gmail.com>
2024-09-05 14:20:29 +0200

    feat: Create task for sending email to user with changed status of order

---
+++ api/apps/orders/tasks.py
@@ -0,0 +1,34 @@
+import logging
+from celery import shared_task
+from django.conf import settings
+from django.core.mail import send_mail
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Order
+
+logger = logging.getLogger("api")
+
+
+@shared_task
+def send_order_change_status_email(order_id):
+    """Send order change status email."""
+    try:
+        order = Order.objects.select_related("user").get(id=order_id)
+        user = order.user
+        recipient = user.email
+        subject = f"Order {order.no} changed status"
+        message = f"Hello {user.username},\n\nYour order {order.no} has changed status to: {order.status}"
+        from_email = settings.DEFAULT_FROM_EMAIL
+
+        send_mail(subject, message, from_email, [recipient])
+        logger.info(
+            f"The email with order {order.no} status changed was sent to {recipient}."
+        )
+
+    except ObjectDoesNotExist:
+        logger.error(f"The order with ID {order_id} does not exist in the database.")
+
+    except Exception as exc:
+        logger.error(
+            f"An error occurred while sending the email with order {order.no}: {str(exc)}"
+        )
+        send_order_change_status_email.retry(exc=exc, countdown=60, max_retries=3)
