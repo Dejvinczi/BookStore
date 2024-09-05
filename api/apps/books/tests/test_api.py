b74a419b1302df11540753405a08fb3023f408da
Dejvinczi <dejvinczi.main@gmail.com>
2024-09-05 21:20:21 +0200

    test: Change logic of some tests to use model ordering and compare with ordered queryset. Also change logic of filtering in some cases to use custom model instance property

--- api/apps/books/tests/test_api.py
+++ api/apps/books/tests/test_api.py
@@ -31,11 +31,11 @@ class TestAuthorViewSet:
     def test_list_with_filter_first_name_success(self, api_client, author_factory):
         """Test that the list of authors can be filtered by first name."""
         author_factory.create_batch(5)
-        filter_author = author_factory(first_name="Non-ExstiningName")
+        custom_author = author_factory(first_name="nonexistingfirstname")
 
         response = api_client.get(
             f"{self.AUTHOR_LIST_URL}",
-            {"first_name": filter_author.first_name},
+            {"first_name": custom_author.first_name},
         )
 
         assert response.status_code == status.HTTP_200_OK
@@ -45,9 +45,7 @@ class TestAuthorViewSet:
     def test_list_with_filter_last_name_success(self, api_client, author_factory):
         """Test that the list of authors can be filtered by last name."""
         author_factory.create_batch(5)
-        custom_author = author_factory(
-            last_name="nonexistinglastname",
-        )
+        custom_author = author_factory(last_name="nonexistinglastname")
 
         response = api_client.get(
             f"{self.AUTHOR_LIST_URL}",
@@ -59,85 +57,106 @@ class TestAuthorViewSet:
 
     def test_list_with_filter_date_of_birth_success(self, api_client, author_factory):
         """Test that the list of authors can be filtered by date of birth."""
-        authors = [
+        for idx in range(1, 6):
             author_factory.create(date_of_birth=f"2000-01-0{idx}")
-            for idx in range(1, 6)
-        ]
-        first_author = authors[0]
+
+        filter_author = author_factory(date_of_birth="1999-01-01")
 
         response = api_client.get(
             f"{self.AUTHOR_LIST_URL}",
             {
-                "date_of_birth_after": first_author.date_of_birth,
-                "date_of_birth_before": first_author.date_of_birth,
+                "date_of_birth_after": filter_author.date_of_birth,
+                "date_of_birth_before": filter_author.date_of_birth,
             },
         )
         assert response.status_code == status.HTTP_200_OK
         assert response.data["count"] == 1
         assert len(response.data["results"]) == 1
 
-    def test_list_with_ordering_by_first_name_success(self, api_client, author_factory):
+    def test_list_with_ordering_by_first_name_success(
+        self, api_client, author_factory, author_model
+    ):
         """Test that the list of authors can be ordered by first name."""
-        authors = author_factory.create_batch(5)
-        ordered_authors = sorted(authors, key=lambda author: author.first_name.lower())
+        author_factory.create_batch(5)
+        ordered_authors = author_model.objects.all().order_by("first_name")
 
         response = api_client.get(
             f"{self.AUTHOR_LIST_URL}",
             {"ordering": "first_name"},
         )
         assert response.status_code == status.HTTP_200_OK
-        assert response.data["results"][0]["id"] == ordered_authors[0].id
+        assert (
+            all(author["id"] for author in response.data["results"]) == author.id
+            for author in ordered_authors
+        )
+
+        ordered_authors = reversed(ordered_authors)
 
         response = api_client.get(
             f"{self.AUTHOR_LIST_URL}",
             {"ordering": "-first_name"},
         )
         assert response.status_code == status.HTTP_200_OK
-        assert response.data["results"][-1]["id"] == ordered_authors[0].id
+        assert (
+            all(author["id"] for author in response.data["results"]) == author.id
+            for author in ordered_authors
+        )
 
-    def test_list_with_ordering_by_last_name_success(self, api_client, author_factory):
+    def test_list_with_ordering_by_last_name_success(
+        self, api_client, author_factory, author_model
+    ):
         """Test that the list of authors can be ordered by last name."""
-        authors = author_factory.create_batch(5)
-        ordered_authors = sorted(authors, key=lambda author: author.last_name)
+        author_factory.create_batch(5)
+        ordered_authors = author_model.objects.all().order_by("last_name")
 
         response = api_client.get(
             f"{self.AUTHOR_LIST_URL}",
             {"ordering": "last_name"},
         )
         assert response.status_code == status.HTTP_200_OK
-        assert [author["id"] for author in response.data["results"]] == [
-            author.id for author in ordered_authors
-        ]
+        assert (
+            all(author["id"] for author in response.data["results"]) == author.id
+            for author in ordered_authors
+        )
 
         response = api_client.get(
             f"{self.AUTHOR_LIST_URL}",
             {"ordering": "-last_name"},
         )
         assert response.status_code == status.HTTP_200_OK
-        assert [author["id"] for author in response.data["results"]] == [
-            author.id for author in reversed(ordered_authors)
-        ]
+        assert (
+            all(author["id"] for author in response.data["results"]) == author.id
+            for author in ordered_authors
+        )
 
     def test_list_with_ordering_by_date_of_birth_success(
-        self, api_client, author_factory
+        self, api_client, author_factory, author_model
     ):
         """Test that the list of authors can be ordered by date of birth."""
-        authors = author_factory.create_batch(5)
-        ordered_authors = sorted(authors, key=lambda author: author.date_of_birth)
+        author_factory.create_batch(5)
+        ordered_authors = author_model.objects.all().order_by("date_of_birth")
 
         response = api_client.get(
             f"{self.AUTHOR_LIST_URL}",
             {"ordering": "date_of_birth"},
         )
         assert response.status_code == status.HTTP_200_OK
-        assert response.data["results"][0]["id"] == ordered_authors[0].id
+        assert (
+            all(author["id"] for author in response.data["results"]) == author.id
+            for author in ordered_authors
+        )
+
+        ordered_authors = reversed(ordered_authors)
 
         response = api_client.get(
             f"{self.AUTHOR_LIST_URL}",
             {"ordering": "-date_of_birth"},
         )
         assert response.status_code == status.HTTP_200_OK
-        assert response.data["results"][-1]["id"] == ordered_authors[0].id
+        assert (
+            all(author["id"] for author in response.data["results"]) == author.id
+            for author in ordered_authors
+        )
 
     def test_retrieve_success(self, api_client, author):
         """Test that an author can be retrieved."""
@@ -243,33 +262,44 @@ class TestGenreViewSet:
     def test_list_with_filter_name_success(self, api_client, genre_factory):
         """Test that the list of genres can be filtered by name."""
         genre_factory.create_batch(5)
-        filter_genre = genre_factory(name="NonExistingName")
+        custom_genre = genre_factory(name="nonexistingname")
+
         response = api_client.get(
             f"{self.GENRE_LIST_URL}",
-            {"name": filter_genre.name},
+            {"name": custom_genre.name},
         )
         assert response.status_code == status.HTTP_200_OK
         assert response.data["count"] == 1
         assert len(response.data["results"]) == 1
 
-    def test_list_with_ordering_by_name_success(self, api_client, genre_factory):
+    def test_list_with_ordering_by_name_success(
+        self, api_client, genre_factory, genre_model
+    ):
         """Test that the list of genres can be ordered by name."""
-        genres = genre_factory.create_batch(5)
-        ordered_genres = sorted(genres, key=lambda genre: genre.name.lower())
+        genre_factory.create_batch(5)
+        ordered_genres = genre_model.objects.all().order_by("name")
 
         response = api_client.get(
             f"{self.GENRE_LIST_URL}",
             {"ordering": "name"},
         )
         assert response.status_code == status.HTTP_200_OK
-        assert response.data["results"][0]["id"] == ordered_genres[0].id
+        assert (
+            all(genre["id"] for genre in response.data["results"]) == genre.id
+            for genre in ordered_genres
+        )
+
+        ordered_genres = reversed(ordered_genres)
 
         response = api_client.get(
             f"{self.GENRE_LIST_URL}",
             {"ordering": "-name"},
         )
         assert response.status_code == status.HTTP_200_OK
-        assert response.data["results"][-1]["id"] == ordered_genres[0].id
+        assert (
+            all(genre["id"] for genre in response.data["results"]) == genre.id
+            for genre in ordered_genres
+        )
 
     def test_retrieve_success(self, api_client, genre):
         """Test that a genre can be retrieved."""
@@ -356,10 +386,11 @@ class TestBookViewSet:
     def test_list_with_filter_title_success(self, api_client, book_factory):
         """Test that the list of books can be filtered by title."""
         book_factory.create_batch(5)
-        filter_book = book_factory(title="NonExistingTitle")
+        custom_book = book_factory(title="nonexistingtitle")
+
         response = api_client.get(
             f"{self.BOOK_LIST_URL}",
-            {"title": filter_book.title},
+            {"title": custom_book.title},
         )
         assert response.status_code == status.HTTP_200_OK
         assert response.data["count"] == 1
@@ -369,41 +400,48 @@ class TestBookViewSet:
         self, api_client, book_factory
     ):
         """Test that the list of books can be filtered by date of publication."""
-        books = [
+        for idx in range(1, 6):
             book_factory.create(publication_date=f"2000-01-0{idx}")
-            for idx in range(1, 6)
-        ]
-        first_book = books[0]
+
+        custom_book = book_factory(publication_date="1999-01-01")
 
         response = api_client.get(
             f"{self.BOOK_LIST_URL}",
             {
-                "publication_date_after": first_book.publication_date,
-                "publication_date_before": first_book.publication_date,
+                "publication_date_after": custom_book.publication_date,
+                "publication_date_before": custom_book.publication_date,
             },
         )
         assert response.status_code == status.HTTP_200_OK
         assert response.data["count"] == 1
         assert len(response.data["results"]) == 1
 
-    def test_list_with_ordering_by_title_success(self, api_client, book_factory):
+    def test_list_with_ordering_by_title_success(
+        self, api_client, book_factory, book_model
+    ):
         """Test that the list of books can be ordered by title."""
-        books = book_factory.create_batch(5)
-        ordered_books = sorted(books, key=lambda genre: genre.title.lower())
+        book_factory.create_batch(5)
+        ordered_books = book_model.objects.all().order_by("title")
 
         response = api_client.get(
             f"{self.BOOK_LIST_URL}",
             {"ordering": "title"},
         )
         assert response.status_code == status.HTTP_200_OK
-        assert response.data["results"][0]["id"] == ordered_books[0].id
+        assert (
+            all(book["id"] for book in response.data["results"]) == book.id
+            for book in ordered_books
+        )
 
         response = api_client.get(
             f"{self.BOOK_LIST_URL}",
             {"ordering": "-title"},
         )
         assert response.status_code == status.HTTP_200_OK
-        assert response.data["results"][-1]["id"] == ordered_books[0].id
+        assert (
+            all(book["id"] for book in response.data["results"]) == book.id
+            for book in ordered_books
+        )
 
     def test_retrieve_success(self, api_client, book):
         """Test that a book can be retrieved."""
