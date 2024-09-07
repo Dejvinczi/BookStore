74176401e6c490f181a73e704964b9003ec3e5e0
Dejvinczi <dejvinczi.main@gmail.com>
2024-09-07 17:27:38 +0200

    feat: Add creation superuser on run scripts

--- api/scripts/test-run.sh
+++ api/scripts/test-run.sh
@@ -3,5 +3,19 @@
 # Apply database migrations
 python manage.py migrate
 
+# Create superuser (if exist - inform user that superuser already exists)
+output=$(python manage.py createsuperuserwithcart --noinput 2>&1) || true
+
+# Check if superuser already exists - if not create it
+if echo "$output" | grep -q "CommandError: Error: That username is already taken."; then
+    echo "Superuser already exists. Skipping creation."
+
+elif [[ "$output" == *"Superuser created successfully."* ]]; then
+    echo "Superuser created successfully."
+else
+    echo "$output"
+    exit 1
+fi
+
 # Run Django development server
 python manage.py runserver 0.0.0.0:8000
