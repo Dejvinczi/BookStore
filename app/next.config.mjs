e6815fc828f0b47c333b05901b6501b5194aff1f
dejvinczi <dejvinczi.main@gmail.com>
2024-09-18 20:33:52 +0200

    refactor: Changes after run prettier formating on app directory

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,26 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: process.env.NODE_ENV === 'production' ? 'standalone' : undefined,
+  output: process.env.NODE_ENV === "production" ? "standalone" : undefined,
 
   // Defines of envs available on client side
   // env: {
-  //   CUSTOM_ENV_VARIABLE: process.env.NODE_ENV === 'production' 
+  //   CUSTOM_ENV_VARIABLE: process.env.NODE_ENV === 'production'
   //     ? 'production_value'
   //     : 'development_value',
   // },
 
   // Function to modify webpack configuration - allows to customize the build process
   webpack: (config, { dev, isServer }) => {
-
-  config.devtool = dev ? 'eval-source-map' : 'source-map'
-
+    config.devtool = dev ? "eval-source-map" : "source-map";
 
     // Place for additional webpack config modifications
     if (!isServer) {
       // Modifications only for client-side code
     }
 
-    return config
+    return config;
   },
 
   // Additional Next.js configuration options
@@ -30,6 +28,6 @@ const nextConfig = {
   // },
 
   poweredByHeader: false, // Disable "X-Powered-By: Next.js" header
-}
+};
 
 export default nextConfig;
