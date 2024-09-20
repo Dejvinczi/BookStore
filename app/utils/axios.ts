d3999642f02492e55860eb3c5b950642717a26a0
dejvinczi <dejvinczi.main@gmail.com>
2024-09-20 14:34:06 +0200

    feat: Add axios to NextJS application

---
+++ app/utils/axios.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api";
+
+const api = axios.create({
+  baseURL: API_URL,
+  timeout: 1000,
+  headers: { "Content-Type": "application/json" },
+});
+
+api.interceptors.request.use(
+  function (config) {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
