b491a316a837c89f735f79256097a46db52557f3
Dejvinczi <dejvinczi.main@gmail.com>
2024-09-24 20:39:18 +0200

    refactor: Change page styles

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,12 +1,8 @@
 export default function HomePage() {
   return (
-    <div className='text-center'>
-      <h1 className='mb-4 text-4xl font-bold text-azure-700'>
-        Welcome to BookStore
-      </h1>
-      <p className='text-lg text-stone-600'>
-        Discover your next favorite book with us!
-      </p>
+    <div className='container mx-auto py-8 px-4 text-center text-light'>
+      <h1 className='text-4xl font-bold mb-4'>Welcome to BookStore</h1>
+      <p className='text-xl'>Discover your next favorite book with us!</p>
     </div>
   );
 }
