26696d1835f74a88d080a13cb35dcc79264bc294
Dejvinczi <dejvinczi.main@gmail.com>
2024-09-24 20:38:57 +0200

    refactor: Change layout design + add AuthProvider

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,27 +1,29 @@
-import type { Metadata } from "next";
+import { AuthProvider } from "@/contexts/AuthContext";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "@/styles/globals.css";
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "BookStore",
-  description: "Your favorite bookstore",
-  icons: {
-    icon: "/favicon.ico",
-  },
+  description: "Your favorite online bookstore",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{ children: React.ReactNode }>) {
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang='en'>
-      <body className='flex min-h-screen flex-col bg-cream-50 text-stone-700'>
-        <Header />
-        <main className='container mx-auto flex-grow px-4 py-8'>
-          {children}
-        </main>
-        <Footer />
+      <body className='bg-secondary text-light flex flex-col min-h-screen'>
+        <AuthProvider>
+          <Header />
+          <main className='flex-grow container mx-auto px-4 py-8'>
+            {children}
+          </main>
+          <Footer />
+        </AuthProvider>
       </body>
     </html>
   );
