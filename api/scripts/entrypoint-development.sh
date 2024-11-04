44f780a0362a3c8c24b5428f9daf0f3571d623d4
dejvinczi <dejvinczi.dev@gmail.com>
2024-11-04 16:50:29 +0100

    chore: Changes after run prettier

--- api/scripts/entrypoint-development.sh
+++ api/scripts/entrypoint-development.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Wait for a db service to be available
-if ! wait-for-it.sh ${DATABASE_HOST}:${DATABASE_PORT} --timeout=30 --strict --; 
+if ! wait-for-it.sh ${DATABASE_HOST}:${DATABASE_PORT} --timeout=30 --strict --;
 then
   echo "Database did not start in time. Exiting."
   exit 1
