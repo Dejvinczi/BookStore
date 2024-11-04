30037421ba099e50a52d962b72133d561ab2e4b0
dejvinczi <dejvinczi.dev@gmail.com>
2024-11-04 15:41:21 +0100

    feat: Add pagination on book page

--- app/app/books/page.tsx
+++ app/app/books/page.tsx
@@ -1,32 +1,60 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import BookList from "@/components/books/BookList";
+import { Pagination } from "@/components/shared/Pagination";
 import { Book, BookApiResponse } from "@/types/book";
 import api from "@/utils/axios";
-import BookList from "@/components/books/BookList";
+import { useEffect, useState } from "react";
 
 export default function BooksPage() {
   const [books, setBooks] = useState<Book[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalItems, setTotalItems] = useState(0);
+  const [hasNext, setHasNext] = useState(false);
+  const [hasPrevious, setHasPrevious] = useState(false);
+  const ITEMS_PER_PAGE = 8;
 
   useEffect(() => {
-    fetchBooks();
-  }, []);
+    fetchBooks(currentPage);
+  }, [currentPage]);
 
-  const fetchBooks = async () => {
+  const fetchBooks = async (page: number) => {
     try {
-      const response = await api.get<BookApiResponse>("/books");
+      const response = await api.get<BookApiResponse>("/books", {
+        params: {
+          limit: ITEMS_PER_PAGE,
+          offset: (page - 1) * ITEMS_PER_PAGE,
+        },
+      });
+
       setBooks(response.data.results);
+      setTotalItems(response.data.count);
+      setHasNext(!!response.data.next);
+      setHasPrevious(!!response.data.previous);
     } catch (error) {
       console.error("Failed to fetch books:", error);
     }
   };
 
+  const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <div className='container mx-auto py-8 px-4'>
-      <h1 className='text-3xl sm:text-4xl font-bold mb-6 text-light'>
-        Available Books
-      </h1>
       <BookList books={books} />
+      {totalPages > 1 && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+          hasNext={hasNext}
+          hasPrevious={hasPrevious}
+        />
+      )}
     </div>
   );
 }
